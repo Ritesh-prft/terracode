f7b6a43a56d2c20bb095ae89e840a5969a915925
Ritesh Khadke <ritesh.khadke@perficient.com>
2021-06-10 21:00:00 +0530

    te

--- vpc-vm.tf
+++ vpc-vm.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "${var.aws_region}"
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
+    access_key = "$AWS_ACCESS_KEY_ID"
+    secret_key = "$AWS_SECRET_ACCESS_KEY"
 }
 
 resource "aws_key_pair" "deployer" {
