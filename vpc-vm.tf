c78a1a2fcdeee477da9bcaeb75883836c3f1d9e8
Ritesh Khadke <rkhadke@gmail.com>
2024-10-31 00:25:09 +0530

    testing tfvars

--- vpc-vm.tf
+++ vpc-vm.tf
@@ -1,16 +1,16 @@
-provider "aws" {
-  region  = "ap-south-1"
-}
-#######################state-path###############################
+# provider "aws" {
+#   region  = "ap-south-1"
+# }
+# #######################state-path###############################
 
-terraform {
-  backend "s3" {
-    bucket = "jenkins-bucket-tfstate"
-    key = "glue-job.tfstate"
-    region = "ap-south-1"
-    encrypt = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "jenkins-bucket-tfstate"
+#     key = "glue-job.tfstate"
+#     region = "ap-south-1"
+#     encrypt = true
+#   }
+# }
 
 
 ################################################################
